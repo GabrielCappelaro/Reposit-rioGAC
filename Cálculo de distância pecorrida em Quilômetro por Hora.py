77bfc7f03c70b59e2008e9d0ee03de4515457960
GabrielCappelaro <167105528+GabrielCappelaro@users.noreply.github.com>
2024-04-15 13:49:58 -0300

    Add files via upload

---
+++ Cálculo de distância pecorrida em Quilômetro por Hora.py
@@ -0,0 +1,12 @@
+velocidadeMetroPorS= 10
+
+velocidadeKMporH= velocidadeMetroPorS * 3.6
+
+tempoDeUmaSemanaEmHoras= 7 * 24 
+
+distancia= velocidadeKMporH * tempoDeUmaSemanaEmHoras
+
+print ("A distância percorrida foi de:", distancia, "Km")
+
+
+
